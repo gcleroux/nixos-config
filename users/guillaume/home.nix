33fee211f92f30b05e83bf14d17ab33b9bd4135a
Guillaume Cleroux <guicleroux@icloud.com>
2023-08-01 13:20:37 -0400

    zsh config

--- users/guillaume/home.nix
+++ users/guillaume/home.nix
@@ -6,6 +6,179 @@
   home.username = "guillaume";
   home.homeDirectory = "/home/guillaume";
 
+  programs.zsh = {
+    enable = true;
+    enableAutosuggestions = true;
+    enableCompletion = true;
+    shellAliases = {
+      icat = "kitty +kitten icat";
+      lg = "lazygit";
+      kssh = "kitty +kitten ssh";
+      v = "nvim";
+      ls = "exa";
+      ll = "exa -alh";
+      tree = "exa --tree";
+      cat = "bat -p";
+      update = "sudo nixos-rebuild switch --flake ~/nixos-config";
+    };
+    history = {
+      size = 10000;
+      path = "$HOME/.zsh_history";
+    };
+    oh-my-zsh = {
+      enable = true;
+      plugins = [ "autojump" ];
+    };
+  };
+
+  programs.starship = {
+      enable = true;
+      enableZshIntegration = true;
+      settings = {
+        aws = {
+          symbol = "  ";
+        };
+        buf = {
+          symbol = " ";
+        };
+        c = {
+          symbol = " ";
+        };
+        conda = {
+          symbol = " ";
+        };
+        dart = {
+          symbol = " ";
+        };
+        directory = {
+          read_only = " 󰌾";
+        };
+        docker_context = {
+          symbol = " ";
+        };
+        elixir = {
+          symbol = " ";
+        };
+        elm = {
+          symbol = " ";
+        };
+        fossil_branch = {
+          symbol = " ";
+        };
+        git_branch = {
+          symbol = " ";
+        };
+        golang = {
+          symbol = " ";
+        };
+        guix_shell = {
+          symbol = " ";
+        };
+        haskell = {
+          symbol = " ";
+        };
+        haxe = {
+          symbol = "⌘ ";
+        };
+        hg_branch = {
+          symbol = " ";
+        };
+        hostname = {
+          ssh_symbol = " ";
+        };
+        java = {
+          symbol = " ";
+        };
+        julia = {
+          symbol = " ";
+        };
+        lua = {
+          symbol = " ";
+        };
+        memory_usage = {
+          symbol = "󰍛 ";
+        };
+        meson = {
+          symbol = "󰔷 ";
+        };
+        nim = {
+          symbol = "󰆥 ";
+        };
+        nix_shell = {
+          symbol = " ";
+        };
+        nodejs = {
+          symbol = " ";
+        };
+        package = {
+          symbol = "󰏗 ";
+        };
+        pijul_channel = {
+          symbol = "🪺 ";
+        };
+        python = {
+          symbol = " ";
+        };
+        rlang = {
+          symbol = "󰟔 ";
+        };
+        ruby = {
+          symbol = " ";
+        };
+        rust = {
+          symbol = " ";
+        };
+        scala = {
+          symbol = " ";
+        };
+        spack = {
+          symbol = "🅢 ";
+        };
+
+        os.symbols = {
+          Alpaquita = " ";
+          Alpine = " ";
+          Amazon = " ";
+          Android = " ";
+          Arch = " ";
+          Artix = " ";
+          CentOS = " ";
+          Debian = " ";
+          DragonFly = " ";
+          Emscripten = " ";
+          EndeavourOS = " ";
+          Fedora = " ";
+          FreeBSD = " ";
+          Garuda = "󰛓 ";
+          Gentoo = " ";
+          HardenedBSD = "󰞌 ";
+          Illumos = "󰈸 ";
+          Linux = " ";
+          Mabox = " ";
+          Macos = " ";
+          Manjaro = " ";
+          Mariner = " ";
+          MidnightBSD = " ";
+          Mint = " ";
+          NetBSD = " ";
+          NixOS = " ";
+          OpenBSD = "󰈺 ";
+          openSUSE = " ";
+          OracleLinux = "󰌷 ";
+          Pop = " ";
+          Raspbian = " ";
+          Redhat = " ";
+          RedHatEnterprise = " ";
+          Redox = "󰀘 ";
+          Solus = "󰠳 ";
+          SUSE = " ";
+          Ubuntu = " ";
+          Unknown = " ";
+          Windows = "󰍲 ";
+        };
+      };
+  };
+
   # This value determines the Home Manager release that your
   # configuration is compatible with. This helps avoid breakage
   # when a new Home Manager release introduces backwards
