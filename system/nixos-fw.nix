53efec7fa4d221b4a9ef5d99b31c553a5ddae682
Guillaume Cleroux <guicleroux@icloud.com>
2023-08-22 11:32:10 -0400

    Added env vars

--- system/nixos-fw.nix
+++ system/nixos-fw.nix
@@ -187,6 +187,13 @@ in {
   # Get completion for system packages
   environment.pathsToLink = [ "/share/zsh" ];
 
+  environment.sessionVariables = {
+    XDG_CACHE_HOME = "$HOME/.cache";
+    XDG_CONFIG_HOME = "$HOME/.config";
+    XDG_DATA_HOME = "$HOME/.local/share";
+    XDG_STATE_HOME = "$HOME/.local/state";
+  };
+
   # Installing fonts
   fonts = {
     fonts = with pkgs; [
