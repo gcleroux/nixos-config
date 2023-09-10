c4639b548d24c68ab03cce990ec82e87b983d64f
Guillaume Cleroux <guicleroux@icloud.com>
2023-09-10 15:32:00 -0400

    Changed mako config

--- users/guillaume/mako.nix
+++ users/guillaume/mako.nix
@@ -1,8 +1,12 @@
 {
   services.mako = {
     enable = true;
-    defaultTimeout = 5;
+    defaultTimeout = 5000;
     anchor = "top-right";
     font = "Cousine Nerd Font Mono 16";
+    extraConfig = ''
+      [category=system]
+      anchor=center
+    '';
   };
 }
