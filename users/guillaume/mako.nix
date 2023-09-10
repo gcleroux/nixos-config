4746bfd7aa17b9d45bdbdef135870cc28fe1177d
Guillaume Cleroux <guicleroux@icloud.com>
2023-09-10 16:46:18 -0400

    New mako settings

--- users/guillaume/mako.nix
+++ users/guillaume/mako.nix
@@ -5,7 +5,10 @@
     anchor = "top-right";
     font = "Cousine Nerd Font Mono 16";
     extraConfig = ''
-      [category=system]
+      [category=overlay]
+      width=200
+      text-alignment=center
+      layer=overlay
       anchor=center
     '';
   };
