7a973ddf8ef245bc052a0661e5ff6a82c90ff143
Guillaume Cleroux <guicleroux@icloud.com>
2023-09-11 14:04:17 -0400

    Fix gtk cursor issues

--- users/guillaume/gtk.nix
+++ users/guillaume/gtk.nix
@@ -4,11 +4,21 @@
     gtk.enable = true;
     package = pkgs.bibata-cursors;
     name = "Bibata-Modern-Ice";
-    size = 32;
+    size = 24;
   };
 
   # GTK theming
   gtk = {
+    enable = true;
+
+    cursorTheme.name = "Bibata-Modern-Ice";
+    cursorTheme.package = pkgs.bibata-cursors;
+    cursorTheme.size = 24;
+
+    font.name = "DejaVu Sans";
+    font.package = pkgs.dejavu_fonts;
+    font.size = 12;
+
     iconTheme.name = "Papirus-Dark";
     iconTheme.package = pkgs.papirus-icon-theme;
 
