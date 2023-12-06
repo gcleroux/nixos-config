b1e827d3effd9985c00afbd127b3ed9b918484d3
Guillaume Cléroux <guillaume.cleroux@protonmail.com>
2023-12-05 23:57:29 -0500

    Delve overlays for neovim debug

--- home/programs/neovim/default.nix
+++ home/programs/neovim/default.nix
@@ -3,9 +3,18 @@
     enable = true;
     defaultEditor = true;
 
-    plugins = with pkgs.vimPlugins; [ nvim-treesitter.withAllGrammars ];
+    plugins = with pkgs.vimPlugins; [
+      nvim-treesitter.withAllGrammars
+      nvim-treesitter-textobjects
+      markdown-preview-nvim
+      vim-markdown-toc
+      go-nvim
+    ];
 
     extraPackages = with pkgs; [
+      # Debugger
+      delve
+
       # Installing tools
       cargo
       luajitPackages.luarocks
@@ -15,6 +24,11 @@
       # Tools packages (Comes with a bunch of stuff)
       clang-tools
       gotools
+      reftools
+      ginkgo
+      richgo
+      gotestsum
+      govulncheck
 
       # LSP packages
       docker-compose-language-service
@@ -48,15 +62,19 @@
       # Formatters
       black
       cbfmt
+      gofumpt
       isort
       nixfmt
       nodePackages.prettier
       shfmt
       stylua
+      golines
 
       # Code actions
       gomodifytags
+      gotests
       impl
+      iferr
     ];
   };
 }
