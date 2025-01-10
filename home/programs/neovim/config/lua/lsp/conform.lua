46858b9d29c3ba49fdf7c7b430a55780dc878a71
Guillaume Cléroux <73357644+gcleroux@users.noreply.github.com>
2025-01-10 16:22:19 -0500

    remove golines

--- home/programs/neovim/config/lua/lsp/conform.lua
+++ home/programs/neovim/config/lua/lsp/conform.lua
@@ -18,7 +18,7 @@ conform.setup({
         cpp = { "clang_format" },
         css = { "prettierd" },
         cuda = { "clang_format" },
-        go = { "gimports-reviser", "gofumpt", "golines" },
+        go = { "gimports-reviser", "gofumpt" },
         html = { "prettierd" },
         javascript = { "prettierd" },
         json = { "prettierd" },
