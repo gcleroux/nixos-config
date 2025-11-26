c8bdefbd5ce0b753a538e7aed0809cd1ee968e78
Guillaume Cléroux <73357644+gcleroux@users.noreply.github.com>
2025-11-25 22:29:44 -0500

    WIP: VLAN config

--- hosts/calcifer/network.nix
+++ hosts/calcifer/network.nix
@@ -58,6 +58,24 @@ in
           Name = "br-lan";
         };
       };
+
+      # VLANs
+      "20-vlan30" = {
+        netdevConfig = {
+          Description = "Guest VLAN";
+          Kind = "vlan";
+          Name = "vlan30";
+        };
+        vlanConfig.Id = 30;
+      };
+      "20-vlan99" = {
+        netdevConfig = {
+          Description = "Management VLAN";
+          Kind = "vlan";
+          Name = "vlan99";
+        };
+        vlanConfig.Id = 99;
+      };
     };
 
     networks = {
@@ -92,11 +110,16 @@ in
         linkConfig.RequiredForOnline = "enslaved";
       };
       "30-lan2" = {
+        # VLAN trunk
         matchConfig.Name = "lan2";
         networkConfig = {
           Bridge = "br-lan";
           ConfigureWithoutCarrier = true;
         };
+        vlan = [
+          "vlan30"
+          "vlan99"
+        ];
         linkConfig.RequiredForOnline = "enslaved";
       };
       "40-br-lan" = {
@@ -109,6 +132,20 @@ in
           IPv4ReversePathFilter = "no";
           ConfigureWithoutCarrier = true;
         };
+      };
+      "40-vlan30" = {
+        matchConfig.Name = "vlan30";
+        address = [
+          "10.0.1.1/24"
+        ];
+        networkConfig.ConfigureWithoutCarrier = true;
+      };
+      "40-vlan99" = {
+        matchConfig.Name = "vlan99";
+        address = [
+          "10.0.255.1/24"
+        ];
+        networkConfig.ConfigureWithoutCarrier = true;
       };
     };
   };
