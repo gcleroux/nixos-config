0adca0b59db81779738a93044fffec56f6712da1
Guillaume Cléroux <73357644+gcleroux@users.noreply.github.com>
2025-12-13 16:45:52 -0500

    calcifer: set static dhcp lease in dnsmasq

--- hosts/calcifer/services/dnsmasq.nix
+++ hosts/calcifer/services/dnsmasq.nix
@@ -29,7 +29,6 @@
         dhcp-range = [ "br-lan,192.168.0.100,192.168.0.250,12h" ];
         dhcp-lease-max = 150;
         interface = "br-lan";
-        dhcp-host = "192.168.0.10";
         port = 1053;
 
         local = "/internal/";
@@ -38,7 +37,16 @@
 
         # Set static IP in DHCP
         no-hosts = true;
-        address = "/${hostname}.internal/192.168.0.10";
+        address = [
+          "/${hostname}.internal/192.168.0.10" # Static IP for router
+          "/home-server/10.0.0.3" # Proxmox node
+        ];
+        dhcp-host = [
+          "192.168.0.10" # Static IP for router
+          "1c:2a:a3:24:04:ac,10.0.0.10,switch" # Smart switch
+          "4c:ea:41:67:19:c8,10.0.0.20,nixos-worker-01" # K8s worker node
+          "b8:2d:28:db:c6:cd,supernote"
+        ];
         dhcp-option = "option:dns-server,192.168.0.10";
       };
     };
