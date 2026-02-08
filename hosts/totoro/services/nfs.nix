1601a702e521ff2fc55459a7547fe5620fdff5db
Guillaume Cléroux <73357644+gcleroux@users.noreply.github.com>
2026-02-08 17:50:22 -0500

    totoro: mount btrfs ssd

--- hosts/totoro/services/nfs.nix
+++ hosts/totoro/services/nfs.nix
@@ -1,6 +1,7 @@
 {
+  # Movies
   fileSystems."/srv/Movies" = {
-    device = "/dev/disk/by-id/ata-ST4000VN008-2DR166_ZDHAL4NL";
+    device = "/dev/disk/by-uuid/717530ad-efae-402a-a397-cae119b2b1d4";
     fsType = "btrfs";
     options = [
       "users"
@@ -16,8 +17,9 @@
     options = [ "bind" ];
   };
 
+  # Shows
   fileSystems."/srv/Shows" = {
-    device = "/dev/disk/by-id/ata-ST4000VN008-2DR166_ZDHAL4NL";
+    device = "/dev/disk/by-uuid/717530ad-efae-402a-a397-cae119b2b1d4";
     fsType = "btrfs";
     options = [
       "users"
@@ -33,8 +35,9 @@
     options = [ "bind" ];
   };
 
+  # Photos
   fileSystems."/srv/Photos" = {
-    device = "/dev/disk/by-id/ata-ST4000VN008-2DR166_ZDHAL4NL";
+    device = "/dev/disk/by-uuid/717530ad-efae-402a-a397-cae119b2b1d4";
     fsType = "btrfs";
     options = [
       "users"
@@ -50,11 +53,49 @@
     options = [ "bind" ];
   };
 
+  # App data
+  fileSystems."/srv/Appdata" = {
+    device = "/dev/disk/by-uuid/c711808d-120c-4c5b-9cf1-07794b061681";
+    fsType = "btrfs";
+    options = [
+      "users"
+      "nofail"
+      "rw"
+      "noatime"
+      "compress=zstd"
+      "subvol=@appdata"
+    ];
+  };
+  fileSystems."/export/Appdata" = {
+    device = "/srv/Appdata";
+    options = [ "bind" ];
+  };
+
+  # Documents
+  fileSystems."/srv/Documents" = {
+    device = "/dev/disk/by-uuid/c711808d-120c-4c5b-9cf1-07794b061681";
+    fsType = "btrfs";
+    options = [
+      "users"
+      "nofail"
+      "rw"
+      "noatime"
+      "compress=zstd"
+      "subvol=@documents"
+    ];
+  };
+  fileSystems."/export/Documents" = {
+    device = "/srv/Documents";
+    options = [ "bind" ];
+  };
+
   services.nfs.server.enable = true;
   services.nfs.server.exports = ''
-    /export         10.0.0.0/24(rw,nohide,insecure,no_subtree_check,crossmnt,fsid=0)
-    /export/Movies  10.0.0.0/24(rw,nohide,insecure,no_subtree_check)
-    /export/Shows   10.0.0.0/24(rw,nohide,insecure,no_subtree_check)
-    /export/Photos  10.0.0.0/24(rw,nohide,insecure,no_subtree_check)
+    /export            10.0.0.0/24(rw,nohide,insecure,no_subtree_check,crossmnt,fsid=0)
+    /export/Appdata    10.0.0.0/24(rw,nohide,insecure,no_subtree_check,no_root_squash)
+    /export/Documents  10.0.0.0/24(rw,nohide,insecure,no_subtree_check,no_root_squash)
+    /export/Movies     10.0.0.0/24(rw,nohide,insecure,no_subtree_check)
+    /export/Photos     10.0.0.0/24(rw,nohide,insecure,no_subtree_check)
+    /export/Shows      10.0.0.0/24(rw,nohide,insecure,no_subtree_check)
   '';
 }
